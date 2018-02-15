4378d22a102f99952edbc27704ba9f36659c1fe1
Chris Pittelko <elda.theelf@gmail.com>
2018-02-15 05:45:00 -0800

    Initial Commit/Test
    Testing Remote getScript for MKB

---
+++ MKB.js
@@ -0,0 +1,8 @@
+console.log('This is a test');
+console.log('Side Loading MKB.js...);
+$(document).ready(function(){
+  console.log('Beginging Load of MKB.js');
+  setTimeout(function(){ 
+    console.log('MKB.js has been loaded!') 
+  }, 3000);
+});
